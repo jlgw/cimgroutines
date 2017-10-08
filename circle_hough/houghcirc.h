abc028f6fccd14c59b20309386676fb20439ecb2
lancebeet <jw@localhost.localdomain>
2017-10-08 23:56:45 +0200

    fixes

--- circle_hough/houghcirc.h
+++ circle_hough/houghcirc.h
@@ -26,9 +26,10 @@ float dist(int x1, int y1, int x2, int y2){
 
 std::vector<int> fn(CImg<unsigned char> img, int min_radius,
         int max_radius, int threshold){
+    //This is slow for large radii, how can it be made more efficient?
     std::vector<int> outv;
     outv.reserve(STEP_CIRCLES);
-    int radius_array[MAX_RADIUS]; //This is faster to search
+    int radius_array[MAX_RADIUS]; //This is faster to search than multidim image
     int width = img.width();
     int height = img.height();
     int xmin, xmax, ymin, ymax;
@@ -40,9 +41,9 @@ std::vector<int> fn(CImg<unsigned char> img, int min_radius,
         xmax = std::min(x1+max_radius, width);
         ymin = std::max(y1-max_radius, 0);
         ymax = std::min(y1+max_radius, height);
-        std::memset(radius_array, 0, max_radius*sizeof(int));
-        for(x2=xmin; x2<xmax; x2++){
-            for(y2=ymin; y2<ymax; y2++){
+        std::memset(radius_array, 0, (max_radius+1)*sizeof(int));
+        for(x2=xmin; x2<=xmax; x2++){
+            for(y2=ymin; y2<=ymax; y2++){
                 if(((int)img(x2,y2))==0){
                     continue;
                 }
@@ -51,7 +52,7 @@ std::vector<int> fn(CImg<unsigned char> img, int min_radius,
                 vote(x1, y1, r, 0)++;
             }
         }
-        im = indmax(&radius_array[min_radius], max_radius-min_radius)+min_radius;
+        im = indmax(&radius_array[min_radius], max_radius-min_radius+1)+min_radius;
         if(radius_array[im]>threshold){
             vote(x1,y1,0,0) = radius_array[im];
         }
@@ -62,11 +63,12 @@ std::vector<int> fn(CImg<unsigned char> img, int min_radius,
             continue;
         }
         if((int)I.max()==(int)(vote(x1,y1))){
-            for(r=min_radius; r<max_radius; r++){
-                //This part is allowed to be slow, it occurs infrequently
+            //There should be a uniqueness check here for edge cases where we 
+            //have two centers with identical number of votes.
+            for(r=min_radius; r<=max_radius; r++){
                 v = (int)vote(x1, y1, r, 0);
                 if(v>threshold){
-                    //Exception handler for too many elements here
+                    //Exception handler for too many elements here(?)
                     if((v>=(int)vote(x1,y1,r-1,0))&&(v>(int)vote(x1,y1,r+1,0))){
                         outv.push_back(x1);
                         outv.push_back(y1);
